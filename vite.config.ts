4870c757cd226d76dd2506b917b882e166b9effc
Alyson <162452483+Alysxn@users.noreply.github.com>
2025-10-28 13:25:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   base: '/bdedu/',
   server: {
     host: "::",
-    port: 8080,
+    port: 80800,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
